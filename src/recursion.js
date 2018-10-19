2b7aba8073fb65ae4e353f7fb10396306da386ab
Tristan Carter <tristancarter@gmail.com>
2018-10-19 16:22:45 -0400

    solution to first two recursion prompts

--- src/recursion.js
+++ src/recursion.js
@@ -7,11 +7,14 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  return n < 0 ? null : n <= 1 ? 1 : n * factorial(n - 1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  return array.length < 1 ? 0 : array.length === 1 
+  ? array[0] : array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
